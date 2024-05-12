36fda76575c7f7830f889330b47cb48d3acfd084
Salem sartawi <107769407+sr6awi@users.noreply.github.com>
2024-05-12 15:33:31 +0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 from PIL import Image
 
 def main():
-    st.header("How old are you according to a Convolution Neural Network? 🥸👧")
+    st.header("How old are you according to My model ? 🥸👧")
     st.write("Upload an image of yourself below to find out! (preferably a squared image and containing only your face)")
     file = st.file_uploader("Upload Photo")
     if file is not None:
         st.image(file, width=300)
         image = Image.open(file)
-        image = tf.image.resize(image, [224,224]) 
+        image = tf.image.resize(image, [128,128]) 
         image = tf.keras.preprocessing.image.img_to_array(image)
         image = image / 255.0      
         image = tf.expand_dims(image, axis=0)
